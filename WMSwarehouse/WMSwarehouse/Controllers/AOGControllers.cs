9ee376fe05ddc93091a84f8b108581f6bd7c1669
HP <HP@DESKTOP-HNEDNFK>
2020-11-16 10:26:20 +0800

    3

---
+++ WMSwarehouse/WMSwarehouse/Controllers/AOGControllers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WMSwarehouse.Controllers
+{
+    public class AOGControllers : Controller
+    {
+        public IActionResult AOGShow()
+        {
+            return View();
+        }
+
+        public IActionResult Deng()
+        {
+            return View();
+        }
+    }
+}
