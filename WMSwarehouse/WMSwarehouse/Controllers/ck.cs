a19b65c098cff6267366fcac798837e93a13909f
快乐三代 <@YANGHAO>
2020-11-20 20:56:49 +0800

    llll

---
+++ WMSwarehouse/WMSwarehouse/Controllers/ck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WMSwarehouse.Controllers
+{
+    public class ck : Controller
+    {
+        //出库明细
+        public IActionResult Outbound()
+        {
+            return View();
+        }
+        //采购退货任务
+        public IActionResult CGreturnedshow()
+        {
+            return View();
+        }
+        //采购退货详情
+        public IActionResult particularsshow()
+        {
+            return View();
+        }
+    }
+}
