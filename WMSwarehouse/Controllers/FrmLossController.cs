fd3d8ebb850208f94a63cc19b50fbcfceb52e75b
文杰 申 <1066321371@qq.com>
2020-11-26 11:11:32 +0800

    lll柳林坡

---
+++ WMSwarehouse/Controllers/FrmLossController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WMSwarehouse.Controllers
+{
+    public class FrmLossController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
