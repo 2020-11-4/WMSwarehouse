06473e36d79a52f1e16b60163b90012aabc9232f
文杰 申 <1066321371@qq.com>
2020-11-26 19:26:32 +0800

    零零零零

--- WMSwarehouse/Controllers/ck.cs
+++ WMSwarehouse/Controllers/ck.cs
@@ -23,5 +23,45 @@ namespace WMSwarehouse.Controllers
         {
             return View();
         }
+        //出库任务单
+        public IActionResult CKout()
+        {
+            return View();
+        }
+        //产品出库任务单详情
+        public IActionResult CPoutshow()
+        {
+            return View();
+        }
+        //产品出库任务单详情
+        public IActionResult CKrecord()
+        {
+            return View();
+        }
+        //添加出库任务
+        public IActionResult outtask()
+        {
+            return View();
+        }
+        //添加盘点任务
+        public IActionResult PDAdd()
+        {
+            return View();
+        }
+        //盘点任务管理未盘点
+        public IActionResult wpdshow()
+        {
+            return View();
+        }
+        //盘点任务管理已盘点
+        public IActionResult ypdshow()
+        {
+            return View();
+        }
+        //未盘点详情
+        public IActionResult xiangqing()
+        {
+            return View();
+        }
     }
 }
